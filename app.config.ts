b7c1e59bae98caa4bc17fe5b3461ca5d9c2a6ac0
erdenetugs <erdenetugsganbaatar@gmail.com>
2023-10-14 23:33:58 +0800

    setup empty project

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  apiKey: 'AIzaSyA5YiaVGD9xj24d2dkQDQ1Q3kicthxUGZI',
+  authDomain: 'challenge-site-ca667.firebaseapp.com',
+  projectId: 'challenge-site-ca667',
+  storageBucket: 'challenge-site-ca667.appspot.com',
+  messagingSenderId: '588308624582',
+  appId: '1:588308624582:web:707a67a3cfd3265cbd13c7',
+  measurementId: 'G-0TM4XS0ZRS',
+});
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <input type="text" />
+      <input type="password" />
+      <button>Sign up</button>
+    </div>
+    <div>
+      <input type="text" />
+      <input type="password" />
+      <button>Sign in</button>
+    </div>
+  </div>
+</template>
